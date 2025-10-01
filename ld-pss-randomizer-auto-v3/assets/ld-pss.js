dbf1920a6f0e6254e4846ba773aa501f4327f8af
Luiz Felipe Januário Fortes <96888435+FelipeJanuario@users.noreply.github.com>
2025-10-01 19:59:31 +0000

    Progress Update

--- ld-pss-randomizer-auto-v3/assets/ld-pss.js
+++ ld-pss-randomizer-auto-v3/assets/ld-pss.js
@@ -12,23 +12,51 @@
             quiz_id: quizId
         }, function(response){
             if (response.success) {
-                var max = response.data.count;
-                var $select = $('#ld-pss-num');
-                $select.empty();
+                    var max = parseInt(response.data.count, 10) || 0;
+                    var $select = $('#ld-pss-num');
+                    $select.empty();
 
-                for (var i = 20; i <= max; i += 10) {
-                    $select.append('<option value="'+i+'">'+i+'</option>');
+                    // Se houver muitas questões, oferecemos opções a partir de 20 em 10 em 10
+                    if (max >= 20) {
+                        for (var i = 20; i <= max; i += 10) {
+                            $select.append('<option value="'+i+'">'+i+'</option>');
+                        }
+                    } else if (max > 0) {
+                        // Se houver menos que 20 questões, geramos opções sensatas de 1 até max
+                        var step = (max <= 10) ? 1 : Math.ceil(max / 5);
+                        for (var j = 1; j <= max; j += step) {
+                            $select.append('<option value="'+j+'">'+j+'</option>');
+                        }
+                        // garantir que o valor máximo esteja presente
+                        if ($select.find('option[value="'+max+'"]').length === 0) {
+                            $select.append('<option value="'+max+'">'+max+'</option>');
+                        }
+                    } else {
+                        $select.append('<option value="">Sem questões disponíveis</option>');
+                        $('#ld-pss-start').prop('disabled', true);
+                    }
+
+                    // guardar max para validação posterior
+                    $select.data('ld-pss-max', max);
+                } else {
+                    var msg = (response.data && response.data.msg) ? response.data.msg : "desconhecido";
+                    if (response.data && response.data.debug) {
+                        msg += '\n\nDebug info: tabelas testadas: ' + (response.data.debug.tables_tested || []).join(', ');
+                    }
+                    alert("Erro ao buscar quantidade de questões: " + msg);
                 }
-            } else {
-                alert("Erro ao buscar quantidade de questões: " + (response.data && response.data.msg ? response.data.msg : "desconhecido"));
-            }
         });
 
         $('#ld-pss-start').on('click', function(e){
             e.preventDefault();
             var val = parseInt($('#ld-pss-num').val(), 10);
-            if (isNaN(val) || val < 20) {
-                alert('Por favor escolha um número válido (mínimo 20).');
+            var maxQuestions = parseInt($('#ld-pss-num').data('ld-pss-max'), 10) || 0;
+            if (isNaN(val) || val <= 0) {
+                alert('Por favor escolha um número válido.');
+                return;
+            }
+            if (maxQuestions > 0 && val > maxQuestions) {
+                alert('O número escolhido é maior que o total de questões disponíveis (' + maxQuestions + ').');
                 return;
             }
 
